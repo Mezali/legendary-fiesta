be26b1c361914a832c27aa90e8a479aa7671e8d1
Daniel de Lima Mazali <daniel.mazali@etec.sp.gov.br>
2021-10-25 08:24:18 -0300

    coisas que o renan apagou...

--- Aula-04-10/login.php
+++ Aula-04-10/login.php
@@ -1,25 +1,44 @@
 <?php
 
-define('HOST', 'localhost'); //PROFESSOR, LEMBRE-SE DE TROCAR OS DADOS
-define('USER', 'root');
-define('PASSWORD', '');
-define('DB', 'banco');
+$local_serve = "localhost";      // local do servidor
+$usuario_serve = "root";         // nome do usuario
+$senha_serve = "";                  // senha
+$banco = "login";      // nome do banco de dados
 
-$conn = mysqli_connect(HOST, USER, PASSWORD, DB) or die("Não foi possivel conectar-se ao banco de dados!");
+$conn = mysqli_connect($local_serve,$usuario_serve,$senha_serve,$banco) or die ("Não foi possivel conectar-se ao banco de dados!");
 
-$sql = "SELECT * FROM user";
+$usuario2 = $_POST['usuario1'];
+$senha2 = md5($_POST['senha1']);
 
-$user = $_POST['usuario'];
-$password = md5($_POST['password']);
+//busca no banco de dados um usuario e senha iguais aos que foram informados
+$sqlbusca = "SELECT * FROM tb_user WHERE usuario = '$usuario2' AND senha = '$senha2'";
 
-$search = "SELECT * FROM user WHERE nome = '$user' AND senha = '$password'";
+//executa o comando e lança o resultado na variavel dados
+$dados = mysqli_query($conn, $sqlbusca) or die("Não foi possível buscar os dados");
 
-$data = mysqli_query($conn, $search) or die("não foi possivel achar os dados >:(");
+//verifica se retornou algum registro do banco
+if (mysqli_num_rows($dados)<=0){ //se o registro é menor ou igual a zero, significa que nao tem usuario e senha cadastrado
+    echo "Usuario nao cadastrado ou dados invalidos";
+}else{ //se tiver usuario e senha cadastrados efetua o login
+    echo "Login efetuado com sucesso!!!!";
+  
+    //aplicação de session
+   if (isset($usuario2)){ //Verifica se a variavel foi criada ou iniciada
+       session_start(); // inicia a sessão
+       $_SESSION['usuario'] = $usuario2; //atribui o usuario informado para a variavel de sessão
+   }
+   header("Location: novo.php"); //Direciona para a pagina restrita
+    
 
-if (mysqli_num_rows($data) <= 0) {
-    echo "Usuario não cadastrado";
-} else {
-    echo "Usuario cadastrado";
-};
+   //aplicação do cookie
+    /*if (isset($usuario2)){  //Verifica se a variavel foi criada ou iniciada
+        setcookie("usuario",$usuario2,time() +60); //cria um cookie com tempo de vida de 1min
+    }
+    $teste = $_COOKIE['usuario'];
+    echo "Usuario logado: $teste";*/
+    
+}
 
-echo '<a href="index.html">Voltar!</a>';
+echo " <a href='login.html'>Voltar</a> ";
+
+?>
